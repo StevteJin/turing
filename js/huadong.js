77f1a5f680b13e1291de08ad261b97a729664eee
金 <1211050148@qq.com>
2019-05-15 21:51:40 +0800

    国际化

--- js/huadong.js
+++ js/huadong.js
@@ -27,15 +27,29 @@ $(function () {
         });
 
     //国际化
-    function loadProperties() {
+    let witchLanguage='en';
+    function loadProperties(language) {
         jQuery.i18n.properties({//加载资浏览器语言对应的资源文件
             name: 'strings', //资源文件名称
             path: '/i18n/', //资源文件路径
             mode: 'map', //用Map的方式使用资源文件中的值
-            language: 'zh',
+            language: language,
             callback: function () {//加载成功后设置显示内容
-                
+                $('.a1').html($.i18n.prop('a1'));
+                $('.a2').html($.i18n.prop('a2'));
+                $('.a3').html($.i18n.prop('a3'));
+                $('.a4').html($.i18n.prop('a4'));
+                $('.a5').html($.i18n.prop('a5'));
+                $('.a6').html($.i18n.prop('a6'));
             }
         });
     }
+    $('.fanyi').on('click',function() {
+        if(witchLanguage=='en'){
+            witchLanguage='zh'
+        }else if(witchLanguage=='zh'){
+            witchLanguage='en'
+        }
+        loadProperties(witchLanguage);
+    })
 })
