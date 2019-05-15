41415d79ddaf0f5cde536b0833ca836ee8b7c28e
金 <1211050148@qq.com>
2019-05-15 21:39:01 +0800

    国际化

--- js/huadong.js
+++ js/huadong.js
@@ -20,9 +20,22 @@ $(function () {
             //console.log('start');
         })
         .setOnDragCallback(function (p) {
-            $(".big-hua-box #content6").css("marginLeft",-p*(bigWidth-1000)+'px')
+            $(".big-hua-box #content6").css("marginLeft", -p * (bigWidth - 1000) + 'px')
         })
         .setOnStopDragCallback(function () {
             //console.log('stop');
         });
+
+    //国际化
+    function loadProperties() {
+        jQuery.i18n.properties({//加载资浏览器语言对应的资源文件
+            name: 'strings', //资源文件名称
+            path: '/i18n/', //资源文件路径
+            mode: 'map', //用Map的方式使用资源文件中的值
+            language: 'zh',
+            callback: function () {//加载成功后设置显示内容
+                
+            }
+        });
+    }
 })
