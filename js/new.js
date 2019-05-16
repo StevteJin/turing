68c0db0e300b3dfa6d2fdf5d3196f9648dfdd76d
金 <1211050148@qq.com>
2019-05-16 19:35:39 +0800

    修改样式

--- js/new.js
+++ js/new.js
@@ -45,7 +45,7 @@ jQuery.i18n.properties({ //加载资浏览器语言对应的资源文件
     name: 'strings', //资源文件名称
     path: '/i18n/', //资源文件路径
     mode: 'map', //用Map的方式使用资源文件中的值
-    language: 'en',
+    language: 'zh',
     callback: function () { //加载成功后设置显示内容
         top_icon = [{
                 class: 'one-click',
